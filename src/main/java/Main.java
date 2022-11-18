b09cb05c8946c4b6245f7469be2c99350e138562
danim <danim@Dani.lan>
2022-11-17 19:44:32 -0500

    My First commit!

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String status ="hungry";
+		if(status == "hungry") {
+			System.out.println("Eat something now!");
+		}else {
+			System.out.println("Enjoy");
+		}
+
+	}
+
+}
